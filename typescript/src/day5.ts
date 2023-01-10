f24f06fb81cadc1f92921d4ff79c44073ded0d6a
sub0ne1 <iam0verread@gmail.com>
2023-01-09 23:35:56 -0500

    day5: conditional types

--- typescript/src/day5.ts
+++ typescript/src/day5.ts
@@ -30,3 +30,23 @@ type age2 = Person1["age"]
 // alias 
 type key ="age"
 type Age1 = Person1[key]  
+
+
+// conditional type
+// Reference: https://www.typescriptlang.org/docs/handbook/2/conditional-types.html
+
+interface Animal {
+  alive():void;
+}
+
+interface Dog extends Animal {
+  woof(): void;
+}
+
+interface Regx{
+
+}
+
+// Condition ? trueExpression : FalseExpression
+type exm1 = Dog extends Animal ? number : string;
+type exm2 = Regx extends Animal ? number  : string;
